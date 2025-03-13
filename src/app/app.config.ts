d11325f7f7e21a59f8747d657120559545e569ed
Chamalfernando <chamaldeshitha2001@gmail.com>
2025-03-13 21:06:02 +0530

    [CHANGES] simple changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 // import { provideGoogleAnalytics } from 'ngx-google-analytics';
-import { environment } from '../environment/environment';
+// import { environment } from '../environment/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
